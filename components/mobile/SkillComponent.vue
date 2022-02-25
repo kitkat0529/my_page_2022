10d41cb7e19b8b753f5e3603f7f75c058e489b3d
Leah Chou <dollars.durarara15@gmail.com>
2022-02-25 16:13:21 +0800

    Update style of skill on mobile

--- components/mobile/SkillComponent.vue
+++ components/mobile/SkillComponent.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-.m-auto(:class="['w-9/12']")
-  .my-10(v-for="skill in skills", data-aos="zoom-in")
-    .text-gray-200.font-mono.text-md.mb-2 - {{ skill.name }}
+.m-auto(:class="['w-7/12']")
+  .mt-10.mb-16.border-2.border-gray-300.p-7.rounded-lg.relative(
+    v-for="skill in skills", data-aos="zoom-in"
+  )
+    .text-gray-200.font-mono.text-md.mb-2.absolute.-top-3.bg-gray-800.px-2 {{ skill.name }}
     .level.italic.text-center.text-green-500.font-mono.text-4xl.font-bold
       span {{ skill.level }}
       span.not-italic.text-gray-500.text-lg.ml-3.font-normal / 10
